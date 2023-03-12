0f1fa74ef82ccf71ac4e6d6f0d46056b82b9cd3e
Wonjin-Lee <claude17@naver.com>
2023-03-12 15:42:26 +0900

    Create Project

---
+++ src/main/java/io/wonjin/springreactivestudy/SpringReactiveStudyApplication.java
@@ -0,0 +1,13 @@
+package io.wonjin.springreactivestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wonjin/springreactivestudy/SpringReactiveStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wonjin.springreactivestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
