9b38bf606b9edcefab456b4fe5c95d89aab4d031
Wonjin-Lee <claude17@naver.com>
2023-05-10 01:54:51 +0900

    Practice Backpressure

--- src/test/java/io/wonjin/springreactivestudy/webflux/backpressure/BackpressureTest.java
+++ src/test/java/io/wonjin/springreactivestudy/webflux/backpressure/BackpressureTest.java
@@ -1,6 +1,8 @@
 package io.wonjin.springreactivestudy.webflux.backpressure;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -19,5 +21,40 @@ public class BackpressureTest {
                 .expectNextCount(3)
                 .thenCancel()
                 .verify();
+    }
+
+    @Test
+    void request10DataTest() {
+        Flux.range(1, 100)
+                .log()
+                .doOnNext(System.out::println)
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription subscription;
+                    private int count;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        this.subscription = s;
+                        this.subscription.request(10);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        count++;
+                        if (count % 10 == 0) {
+                            this.subscription.request(10);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
